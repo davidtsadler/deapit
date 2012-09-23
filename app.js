c610b27a5906cbdee70f41b1bad2d89f1050d464
davidtsadler <davidtsadler@googlemail.com>
2012-09-23 14:50:03 +0100

    Basic app running.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+	,routes = require('./routes')
+	,http = require('http');
+
+var app = express();
+
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.static(__dirname + '/public'));
+	app.use(app.router);
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(3000);
+
+console.log('DEAPIT server listening on port 3000');
