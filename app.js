78d7012515ffecdcfe8fad437f428c640fd2a11d
davidtsadler <davidtsadler@googlemail.com>
2012-09-23 18:20:49 +0100

    Default field values specified for GET request. POST request values load fields.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express')
-	,routes = require('./routes')
-	,http = require('http');
+var express = require('express'),
+	routes = require('./routes'),
+	http = require('http');
 
 var app = express();
 
-app.configure(function(){
+app.configure(function () {
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'ejs');
 	app.use(express.favicon('public/favicon.ico'));
 	app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
 	app.use(app.router);
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
-exports.index = function(req, res){
-	res.render('index');
+var requestDefaults = {
+	endpoint : 'http://open.api.sandbox.ebay.com/shopping?',
+	headers : 'X-EBAY-API-APP-ID:<ENTER YOUR APP ID>\r\nX-EBAY-API-VERSION:791\r\nX-EBAY-API-SITE-ID:0\r\nX-EBAY-API-CALL-NAME:GeteBayTime\r\nX-EBAY-API-REQUEST-ENCODING:XML',
+	body : '<?xml version="1.0" encoding="utf-8"?>\r\n<GeteBayTimeRequest xmlns="urn:ebay:apis:eBLBaseComponents">\r\n</GeteBayTimeRequest>'
+};
+
+exports.index = function(req, res) {
+	var request = req.route.method === 'get' ? requestDefaults : req.body.request;
+	res.render('index', { request : request });
 };
