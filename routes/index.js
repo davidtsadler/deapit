b076267c2237bca4b2eb5274f96db49559c09212
davidtsadler <davidtsadler@googlemail.com>
2012-09-26 21:59:37 +0100

    Add the ability to HTTP requests to the eBay API. Response is shown.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
-var requestDefaults = {
-	endpoint : 'http://open.api.sandbox.ebay.com/shopping?',
-	headers : 'X-EBAY-API-APP-ID:<ENTER YOUR APP ID>\r\nX-EBAY-API-VERSION:791\r\nX-EBAY-API-SITE-ID:0\r\nX-EBAY-API-CALL-NAME:GeteBayTime\r\nX-EBAY-API-REQUEST-ENCODING:XML',
-	body : '<?xml version="1.0" encoding="utf-8"?>\r\n<GeteBayTimeRequest xmlns="urn:ebay:apis:eBLBaseComponents">\r\n</GeteBayTimeRequest>'
-};
+var ebay = require('../lib/ebay');
 
-exports.index = function(req, res) {
-	var request = req.route.method === 'get' ? requestDefaults : req.body.request;
-	res.render('index', { request : request });
+var apiReqDefaults = {
+		endpoint : 'http://open.api.sandbox.ebay.com/shopping?',
+
+		headers : [
+			'X-EBAY-API-APP-ID:<ENTER YOUR APP ID>',
+			'X-EBAY-API-VERSION:791',
+			'X-EBAY-API-SITE-ID:0',
+			'X-EBAY-API-CALL-NAME:GeteBayTime',
+			'X-EBAY-API-REQUEST-ENCODING:XML'
+		].join('\r\n'),
+
+		body : [
+			'<?xml version="1.0" encoding="utf-8"?>',
+			'<GeteBayTimeRequest xmlns="urn:ebay:apis:eBLBaseComponents">',
+			'</GeteBayTimeRequest>',
+		].join('\r\n')
+	};
+
+exports.index = function (req, res) {
+	if (req.route.method === 'get') {
+		res.render('index', { apiReq : apiReqDefaults, apiRes : null });
+	}
+	else {
+		ebay.request(req.body.apiReq, function (error, apiRes) {
+			if (!error) {
+				res.render('index', { apiReq : req.body.apiReq, apiRes : apiRes });
+			}
+			else {
+				res.render('index', { apiReq : req.body.apiReq, apiRes : { body : error.message } });
+			}
+		});
+	}
 };
